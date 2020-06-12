96d740b31ed3953043d85381df20bfa41eaa895b
andrepucas <andre.pucas.santos@gmail.com>
2020-06-12 17:30:50 +0100

    Add separate functions to update problems

--- imfj2_pf.py
+++ imfj2_pf.py
@@ -12,8 +12,7 @@ TXT_MENU = """
 ||
 || (help): Type <help> for additional information.
 ||
-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-"""
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"""
 
 TXT_FLOAT = """
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -29,12 +28,11 @@ TXT_FLOAT = """
 || <fluid> density = {3} kg/m^3 
 || <gravity>       = {4} m/s
 ||
-|| From it's center, the cube floats at: {5} m.
+|| From it's center, the cube sinks: {5} m.
 || 
 || (help): Type <help> for additional information.
 ||
-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-"""
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"""
 
 TXT_SPRING = """
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -53,8 +51,7 @@ TXT_SPRING = """
 || 
 || (help): Type <help> for additional information.
 ||
-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-"""
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"""
 
 # Help messages
 TXT_HELP_M = """
@@ -64,8 +61,7 @@ TXT_HELP_M = """
 ??         -> <quit> at any time closes the program.
 ??         -> <menu> shows you the menu again.
 ??
-???????????????????????????????????????????????????????????????????
-"""
+???????????????????????????????????????????????????????????????????"""
 
 TXT_HELP_F = """
 ???????????????????????????????????????????????????????????????????
@@ -75,8 +71,7 @@ TXT_HELP_F = """
 ??         -> <quit> closes the program.
 ??         -> <menu> returns you to the main menu.
 ??
-???????????????????????????????????????????????????????????????????
-"""
+???????????????????????????????????????????????????????????????????"""
 
 TXT_HELP_S = """
 ???????????????????????????????????????????????????????????????????
@@ -86,8 +81,7 @@ TXT_HELP_S = """
 ??         -> <quit> closes the program.
 ??         -> <menu> returns you to the main menu.
 ??
-???????????????????????????????????????????????????????????????????
-"""
+???????????????????????????????????????????????????????????????????"""
 
 # Error message
 TXT_ERROR = """
@@ -95,8 +89,7 @@ TXT_ERROR = """
 !!
 !! (error): -> Invalid inputs. Failed to understand what you want.
 !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-"""
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"""
 
 ### Menu
 def menu():
@@ -105,6 +98,7 @@ def menu():
     # Waits for user to input one of the options
     user_i = ""
     while (user_i != "quit"):
+        print("")
         user_i = input(">> ")
         user_i = user_i.strip().lower()
 
@@ -138,19 +132,11 @@ def floatation():
     edge = volume ** (1/3)
     center = edge / 2
 
+    # Appears before while loop to show an example to the user 
+    updateF(cube, mass, volume, fluid, gravity, edge, center)
+
     while True:
-
-        # RELATIVE DEPTH WITH CENTER OF THE CUBE - EXPLAINED
-        # Gravitational Force = Buoyancy Force <=>
-        # mass * gravity = fluid * gravity * volumeSubmerged <=>
-        # mass = fluid * volumeSubmerged <=>
-        volumeSubmerged = mass / fluid
-        # volumeSubmerged = edge * edge * edgeSubmerged <=>
-        edgeSubmerged = volumeSubmerged / (edge * edge)
-        depth = edgeSubmerged - center
-
-        print(TXT_FLOAT.format(cube, mass, volume, fluid, gravity, depth))
-
+        print("")
         user_i = input(">> ")
         user_i = user_i.strip().lower().split()
 
@@ -161,26 +147,31 @@ def floatation():
                 cube = float(user_i[2])
                 # Mass needs to be updated
                 mass = cube * volume
+                updateF(cube, mass, volume, fluid, gravity, edge, center)
 
             # Set mass
             elif (user_i[1] == "mass"):
                 mass = float(user_i[2])
                 # Volume needs to be updated
                 volume = mass / cube
+                updateF(cube, mass, volume, fluid, gravity, edge, center)
 
             # Set volume
             elif (user_i[1] == "volume"):
                 volume = float(user_i[2])
                 # Mass needs to be updated
                 mass = cube * volume
+                updateF(cube, mass, volume, fluid, gravity, edge, center)
 
             # Set fluid density
             elif (user_i[1] == "fluid"):
                 fluid = float(user_i[2])
+                updateF(cube, mass, volume, fluid, gravity, edge, center)
 
             # Set gravity
             elif (user_i[1] == "gravity"):
                 gravity = float(user_i[2])
+                updateF(cube, mass, volume, fluid, gravity, edge, center)
 
             # Error box
             else:
@@ -206,26 +197,36 @@ def floatation():
             print(TXT_ERROR)
             print(TXT_HELP_F)
 
+### Updates Floatation Board - A separate function for this allows for the 
+### board to only be updated when new values are inserted, instead of every
+### cycle, which resulted in error/help messages staying hidden behind a
+### repeated board.
+def updateF(cube, mass, volume, fluid, gravity, edge, center):
+    # RELATIVE DEPTH WITH CENTER OF THE CUBE - EXPLAINED
+    # Gravitational Force = Buoyancy Force <=>
+    # mass * gravity = fluid * gravity * volumeSubmerged <=>
+    # mass = fluid * volumeSubmerged <=>
+    volumeSubmerged = mass / fluid
+    # volumeSubmerged = edge * edge * edgeSubmerged <=>
+    edgeSubmerged = volumeSubmerged / (edge * edge)
+    depth = edgeSubmerged - center
+
+    print(TXT_FLOAT.format(cube, mass, volume, fluid, gravity, depth))
+
+
 ### Springs problem
 def springs():
-    rest = 0.25
-    constant = 25
-    mass = 0.5
+    rest = 0.2
+    constant = 50
+    mass = 2
     gravity = 9.81
-    gF = 4.905
+    gF = mass * gravity
+
+    # Appears before while loop to show an example to the user 
+    updateS(rest, constant, mass, gravity, gF)
 
     while True:
-
-        # SPRING VERTICAL STRETCH - EXPLAINED
-        # Gravitational Force + Spring Force = 0 <=>
-        # - gravity * mass + (-constant * (stretch + rest)) = 0 <=>
-        # - gF - (constant * stretch) - (constant * rest) = 0 <=>
-        # - (constant * stretch) = gF + (constant * rest) <=>
-        # - stretch = (gF + (constant*rest)) / - constant <=>
-        stretch = (gF + (constant*rest)) / constant
-
-        print(TXT_SPRING.format(rest, constant, mass, gravity, stretch))
-
+        print("")
         user_i = input(">> ")
         user_i = user_i.strip().lower().split()
 
@@ -234,22 +235,26 @@ def springs():
             # Set spring rest length
             if (user_i[1] == "rest"):
                 rest = float(user_i[2])
+                updateS(rest, constant, mass, gravity, gF)
 
             # Set spring constant
             elif (user_i[1] == "constant"):
                 constant = float(user_i[2])
+                updateS(rest, constant, mass, gravity, gF)
 
             # Set object mass
             elif (user_i[1] == "mass"):
                 mass = float(user_i[2])
                 # gF needs to be updated
                 gF = mass * gravity
+                updateS(rest, constant, mass, gravity, gF)
 
             # Set gravity
             elif (user_i[1] == "gravity"):
                 gravity = float(user_i[2])
                 # gF needs to be updated
                 gF = mass * gravity
+                updateS(rest, constant, mass, gravity, gF)
 
             # Error box
             else:
@@ -274,6 +279,19 @@ def springs():
         else:
             print(TXT_ERROR)
             print(TXT_HELP_S)
+
+### Updates Springs Board - Just like updateF(), this allows for error/help 
+### messages to not get buried under a new repeated values board.
+def updateS(rest, constant, mass, gravity, gF):
+    # SPRING VERTICAL STRETCH - EXPLAINED
+    # Gravitational Force + Spring Force = 0 <=>
+    # - gravity * mass + (-constant * (stretch + rest)) = 0 <=>
+    # - gF - (constant * stretch) - (constant * rest) = 0 <=>
+    # - (constant * stretch) = gF + (constant * rest) <=>
+    # - stretch = (gF + (constant*rest)) / - constant <=>
+    stretch = (gF + (constant*rest)) / constant
+
+    print(TXT_SPRING.format(rest, constant, mass, gravity, stretch))
 
 # Program starts running here
 menu()
