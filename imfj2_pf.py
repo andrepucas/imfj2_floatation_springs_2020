a2de6b2dc4bdfae81a14a11e9e06d0b7c5c420bf
andrepucas <andre.pucas.santos@gmail.com>
2020-06-12 03:28:40 +0100

    Finish floatation problem + beautification

--- imfj2_pf.py
+++ imfj2_pf.py
@@ -2,37 +2,72 @@
 # 2º Semestre 2019/2020
 # André Santos 21901767
 
-# Strings
+# Main messages
 TXT_MENU = """
-What problem do you want to solve?
-1. Floatation
-2. Springs
-
-(help): You can type in <quit> at any time to close the program 
-        or <menu> to return to this menu.
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||
+|| What problem do you want to solve?
+|| 1. Floatation
+|| 2. Springs
+||
+|| (help): Type <help> for additional information.
+||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 """
+
 TXT_FLOAT = """
-Current simulation parameters: 
-Object: Mass = {0} kg | Density = {1} kg/m^3 | Volume = {2} m^3
-Environment: Fluid Density = {3} kg/m^3 | Gravity = {4}
-
-Object would float at: {5}
-
-(help): Type <help> for additional information.
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||
+|| FLOATATION - CURRENT PARAMETERS:
+||
+|| CUBE:
+|| <cube> density  = {0} kg/m^3 
+|| <mass>          = {1} kg 
+|| <volume>        = {2} m^3
+||
+|| FLUID AND GRAVITY:
+|| <fluid> density = {3} kg/m^3 
+|| <gravity>       = {4} m/s
+||
+|| From it's center, the cube floats at: {5} m.
+|| 
+|| (help): Type <help> for additional information.
+||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 """
-TXT_HELP_FLOAT = """
-(help): Typing <set fluid 5> updates the fluid density to 5. 
-        Typing <set volume 10, gravity 15> updates both volume and  
-        gravity simultaneously. 
-        
-        !! You are only allowed to change up to 2 object related 
-        parameters (mass/density/volume) at a time !!
 
-        You can also type <menu> to return to the main menu or 
-        <quit> to close the program.
+# Help messages
+TXT_HELP_M = """
+???????????????????????????????????????????????????????????????????
+??
+?? (help): -> Type in <1> or <2> and press Enter to choose.
+??         -> <quit> at any time closes the program.
+??         -> <menu> shows you the menu again.
+??
+???????????????????????????????????????????????????????????????????
 """
 
-# Menu
+TXT_HELP_F = """
+???????????????????????????????????????????????????????????????????
+??
+?? (help): -> Examples: 
+??         -> <set fluid 500> sets fluid value to 500.
+??         -> <quit> closes the program.
+??         -> <menu> returns you to the main menu.
+??
+???????????????????????????????????????????????????????????????????
+"""
+
+# Error message
+TXT_ERROR = """
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!! (error): -> Invalid inputs. Failed to understand what you want.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""
+
+### Menu
 def menu():
     print(TXT_MENU)
 
@@ -42,46 +77,105 @@ def menu():
         user_i = input(">> ")
         user_i = user_i.strip().lower()
 
+        # Starts floatation problem
         if (user_i[0] == "1" or user_i == "floatation"):
             floatation()
 
+        # Starts springs problem
         elif (user_i[0] == "2" or user_i == "springs"):
             springs()
 
+        # Help box
+        elif (user_i == "help"):
+            print(TXT_HELP_M)
+
+        # Shows this menu again
         elif (user_i == "menu"):
             print(TXT_MENU)
 
+        # Ignores mistakes
         else:
             continue
 
+### Flotation problem
 def floatation():
-    mass = 250
-    density = 500
-    volume = 0.5
+    cube = 500
+    mass = 4000
+    volume = 8
     fluid = 1000
     gravity = 9.81
-    depth = 0
+    edge = volume ** (1/3)
+    center = edge / 2
 
-    user_i = ""
     while True:
-        print(TXT_FLOAT.format(mass, density, volume, fluid, gravity, depth))
-        
+
+        # RELATIVE DEPTH WITH CENTER OF THE CUBE EXPLAINED
+        # Gravitational Force = Buoyancy Force <=>
+        # mass * gravity = fluid * gravity * volumeSubmerged <=>
+        # mass = fluid * volumeSubmerged <=>
+        volumeSubmerged = mass / fluid
+        # volumeSubmerged = edge * edge * edgeSubmerged <=>
+        edgeSubmerged = volumeSubmerged / (edge * edge)
+        depth = edgeSubmerged - center
+
+        print(TXT_FLOAT.format(cube, mass, volume, fluid, gravity, depth))
+
         user_i = input(">> ")
         user_i = user_i.strip().lower().split()
 
         if (user_i[0] == "set"):
-            if (user_i[1] == "mass"):
-                mass = user_i[2]
 
+            # Set cube density
+            if (user_i[1] == "cube"):
+                cube = float(user_i[2])
+                # Mass needs to be updated
+                mass = cube * volume
+
+            # Set mass
+            elif (user_i[1] == "mass"):
+                mass = float(user_i[2])
+                # Volume needs to be updated
+                volume = mass / cube
+
+            # Set volume
+            elif (user_i[1] == "volume"):
+                volume = float(user_i[2])
+                # Mass needs to be updated
+                mass = cube * volume
+
+            # Set fluid density
+            elif (user_i[1] == "fluid"):
+                fluid = float(user_i[2])
+
+            # Set gravity
+            elif (user_i[1] == "gravity"):
+                gravity = float(user_i[2])
+
+            # Error box
+            else:
+                print(TXT_ERROR)
+                print(TXT_HELP_F)
+
+        # Help box
         elif (user_i[0] == "help"):
-            print(TXT_HELP_FLOAT)
+            print(TXT_HELP_F)
 
+        # Returns to menu
         elif (user_i[0] == "menu"):
             menu()
+            # exit() in case the user goes to the menu and types quit there
+            exit()
 
+        # Closes program
         elif (user_i[0] == "quit"):
             exit()
 
+        # Error box
+        else:
+            print(TXT_ERROR)
+            print(TXT_HELP_F)
+
+### Springs problem
 def springs():
     print("springs option")
 
